2b98722777250661862d0cdb46a12d7265839c00
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-12 14:23:20 +0200

    Person - Übung

---
+++ Taschenrechner_Unittests/Taschenrechner.Tests/PersonTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner.Tests
+{
+    [TestClass]
+    public class PersonTests
+    {
+        [TestMethod]
+        public void Equals_with_null_returns_false()
+        {
+            Person p1 = new Person { Vorname = "Tom", Nachname = "Ate", Alter = 10, Kontostand = 100 };
+            Person p2 = null;
+
+           Assert.IsFalse(p1.Equals(p2));
+        }
+
+        [TestMethod]
+        public void Equals_with_shallow_copy_returns_true()
+        {
+            Person p1 = new Person { Vorname = "Tom", Nachname = "Ate", Alter = 10, Kontostand = 100 };
+            Person p2 = p1; // Referenzkopie
+
+            Assert.IsTrue(p1.Equals(p2));
+        }
+
+        [TestMethod]
+        public void Equals_with_hard_copy_returns_true()
+        {
+            Person p1 = new Person { Vorname = "Tom", Nachname = "Ate", Alter = 10, Kontostand = 100 };
+            Person p2 = new Person { Vorname = "Tom", Nachname = "Ate", Alter = 10, Kontostand = 100 };
+
+            Assert.IsTrue(p1.Equals(p2));
+        }
+        [TestMethod]
+        public void Equals_with_wrong_person_returns_false()
+        {
+            Person p1 = new Person { Vorname = "Tom", Nachname = "Ate", Alter = 10, Kontostand = 100 };
+            Person p2 = new Person { Vorname = "Anna", Nachname = "Nass", Alter = 10, Kontostand = 100 };
+
+            Assert.IsFalse(p1.Equals(p2));
+        }
+        [TestMethod]
+        public void Equals_with_wrong_type_returns_false()
+        {
+            Person p1 = new Person { Vorname = "Tom", Nachname = "Ate", Alter = 10, Kontostand = 100 };
+            StringBuilder sb = new StringBuilder();
+
+            Assert.IsFalse(p1.Equals(sb));
+        }
+    }
+}
---
+++ Taschenrechner_Unittests/Taschenrechner/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    public class Person
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public byte Alter { get; set; }
+        public decimal Kontostand { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) // C#7
+                return false;
+            if (obj is Person == false)
+                return false;
+
+            Person p2 = (Person)obj;
+
+            if (this == p2) // == -> Referenzvergleich
+                return true;
+            else // Wertevergleich
+            {
+                if (this.Vorname == p2.Vorname &&
+                    this.Nachname == p2.Nachname &&
+                    this.Alter == p2.Alter &&
+                    this.Kontostand == p2.Kontostand)
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+    }
+}
