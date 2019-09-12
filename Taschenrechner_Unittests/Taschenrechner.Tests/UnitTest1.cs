ebed724543763f9e144dc21c6e4485d568a3a0c7
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-12 10:51:01 +0200

    Normalfall getestet

--- Taschenrechner_Unittests/Taschenrechner.Tests/UnitTest1.cs
+++ Taschenrechner_Unittests/Taschenrechner.Tests/UnitTest1.cs
@@ -4,11 +4,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Taschenrechner.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class RechnerTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_2_and_3_results_5()
         {
+            // Arrange
+            Rechner rechner = new Rechner();
+
+            // Action
+            var result = rechner.Add(2, 3);
+
+            // Assert
+            Assert.AreEqual(5, result);
         }
     }
 }
