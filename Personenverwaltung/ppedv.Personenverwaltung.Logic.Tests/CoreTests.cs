14380af7a9fb8a1793bf3d2a841ba57d52c802d4
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-13 11:55:07 +0200

    Problem: Test mit Hardware als Abhängigkeit

--- Personenverwaltung/ppedv.Personenverwaltung.Logic.Tests/CoreTests.cs
+++ Personenverwaltung/ppedv.Personenverwaltung.Logic.Tests/CoreTests.cs
@@ -1,13 +1,24 @@
-﻿using System;
+﻿using ppedv.Personenverwaltung.Logik;
+using ppedv.PersonenVerwaltung.Logic.Hardware.RoboTech;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace ppedv.Personenverwaltung.Logic.Tests
 {
     public class CoreTests
     {
+        [Fact]
+        public void RecruitPersonForDepartment_Can_Recruit_5_Persons()
+        {
+            Core core = new Core(new XingRecruiter3000());
 
+            var result = core.RecruitPersonForDepartment(5);
+
+            Assert.Equal(5, result.Count());
+        }
     }
 }
