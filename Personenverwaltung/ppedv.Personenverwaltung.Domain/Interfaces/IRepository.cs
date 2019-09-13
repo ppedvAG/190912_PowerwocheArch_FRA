441fbc14e51c4ced94a943e66ae9eed1e5acd266
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-13 14:18:33 +0200

    Interface für die DB-Layer erstellt

---
+++ Personenverwaltung/ppedv.Personenverwaltung.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.Personenverwaltung.Domain.Interfaces
+{
+    public interface IRepository
+    {
+        void Add<T>(T item);
+        void Delete<T>(T item);
+        void Update<T>(T item);
+        T GetByID<T>(int ID);
+        IEnumerable<T> GetAll<T>();
+        void Save();
+    }
+}
