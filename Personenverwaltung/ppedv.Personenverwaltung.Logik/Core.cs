f284fb721c32e3dcc5091ebf92615816c66f24a6
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-13 11:48:25 +0200

    Core erstellt

---
+++ Personenverwaltung/ppedv.Personenverwaltung.Logik/Core.cs
@@ -0,0 +1,30 @@
+﻿using ppedv.Personenverwaltung.Domain;
+using ppedv.Personenverwaltung.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.Personenverwaltung.Logik
+{
+    public class Core
+    {
+        public Core(IDevice device)
+        {
+            this.device = device;
+        }
+        private IDevice device;
+
+        // In der Software wird die Hardware angesteuert
+        public IEnumerable<Person> RecruitPersonForDepartment(int amount)
+        {
+            List<Person> newMembers = new List<Person>();
+            for (int i = 0; i < amount; i++)
+            {
+                newMembers.Add(device.RecruitPerson());
+            }
+            return newMembers;
+        }
+    }
+}
