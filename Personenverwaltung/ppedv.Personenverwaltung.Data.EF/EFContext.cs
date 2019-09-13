7287060be68f5daf8eb5c98a573dcf51aff2f97e
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-13 10:41:27 +0200

    Basisversion von EF erstellt

---
+++ Personenverwaltung/ppedv.Personenverwaltung.Data.EF/EFContext.cs
@@ -0,0 +1,21 @@
+﻿using ppedv.Personenverwaltung.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.Personenverwaltung.Data.EF
+{
+    public class EFContext : DbContext
+    {
+        // Minimalkonfiguration
+        public EFContext() : this(@"Server=(localdb)\MSSQLLocalDB;Database=Personenverwaltung_Produktion;Trusted_Connection=true;C:\temp\Personenverwaltung.mdf") {}
+        public EFContext(string connectionString) : base(connectionString){}
+
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Company> Company { get; set; }
+    }
+}
