26666a0a82adf7de805558a74af3e61e1b3da5cf
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-13 11:45:16 +0200

    UnitTest für Hardware

--- Personenverwaltung/ppedv.Personenverwaltung.Logic.Hardware.RoboTech.Tests/Class1.cs
+++ Personenverwaltung/ppedv.Personenverwaltung.Logic.Hardware.RoboTech.Tests/Class1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework;
+using ppedv.PersonenVerwaltung.Logic.Hardware.RoboTech;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace ppedv.Personenverwaltung.Logic.Hardware.RoboTech.Tests
 {
-    public class Class1
+    [TestFixture]
+    public class XingRecruiter3000Tests
     {
+        [Test]
+        public void Can_Create_Person()
+        {
+            var recruiter = new XingRecruiter3000();
+            var person = recruiter.RecruitPerson();
+            Assert.NotNull(person);
+        }
     }
 }
