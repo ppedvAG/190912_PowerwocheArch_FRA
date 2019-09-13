c992f46200c5ecd64d898e9f26bbfa02202b687b
Michael Zöhling <michael.zoehling@outlook.com>
2019-09-13 10:43:33 +0200

    Init EF.Tests

---
+++ Personenverwaltung/ppedv.Personenverwaltung.Data.EF.Tests/EFContextTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ppedv.Personenverwaltung.Data.EF.Tests
+{
+    [TestClass]
+    public class EFContextTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Personenverwaltung/ppedv.Personenverwaltung.Data.EF.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ppedv.Personenverwaltung.Data.EF.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ppedv.Personenverwaltung.Data.EF.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c818e331-59f4-44ad-885c-a59c0935b4fa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
